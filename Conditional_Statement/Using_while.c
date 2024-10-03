06983bccd9fb235f119f25196e8732fa223e2642
PrachiJadhav14 <prachijadhav5514@gmail.com>
2024-10-03 14:31:11 +0530

    while.c

---
+++ Conditional_Statement/Using_while.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 0;
+    while(i < 10)
+    {
+        printf("\n %d Welcome",i+10);
+        i++;
+    }
+    printf("\n\n Hello World !!");
+    printf("\n Thanks!!!!!!");
+    return 0;
+}
