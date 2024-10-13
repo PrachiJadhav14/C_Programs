e92c8b88d021fec05f56842c8dfa87efcd7cc8a4
PrachiJadhav14 <prachijadhav5514@gmail.com>
2024-10-13 14:29:24 +0530

    Table Program

---
+++ Table/01Table.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No = 0, Cnt = 0;
+
+    printf("\n Enter Any No =>");
+    scanf("%d",&No);
+
+    printf("\n==================*******************===================");
+
+
+    for(Cnt=1;Cnt<=10;Cnt++)
+    printf("\n\t %3d *%3d = %3d",No,Cnt,(No*Cnt));
+
+
+    printf("\n==================*******************===================");
+
+
+    getch();
+    return 0;
+
+}
