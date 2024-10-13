ccca1bc2b9b5c990cf5d436bd5180bf4f03e5c7f
PrachiJadhav14 <prachijadhav5514@gmail.com>
2024-10-13 14:15:09 +0530

    Digit Program

--- Digit Program/Program To Print Maximum Digit in Inputed No.c
+++ Digit Program/Program To Print Maximum Digit in Inputed No.c
@@ -1,32 +1,30 @@
-
 #include<stdio.h>
 #include<conio.h>
 int main()
-
 {
     int No = 0, Temp = 0, Dig = 0, Max = 0;
 
-    printf("\n Enter Positive No :");
+
+
+    printf("\n Enter Any Positive No => ");
     scanf("%d",&No);
-    Temp=No;
 
+    Temp = No;
 
-    // Max=Temp%10;//
+   // Max =Temp%10
 
+   while(Temp>0)
 
-    while(Temp>0)
+   {
+      Dig = Temp %10;
+      if(Dig>Max)
+      {
+          Max = Dig;
+      }
+      Temp=Temp/10;
+   }
+   printf("\n Count Maximum is %d = %d",No,Max);
 
-    {
-        Dig=Temp%10;
-        if(Dig>Max)
-        {
-            Max=Dig;
-
-        }
-        Temp=Temp/10;
-    }
-    printf("\n Count Maximum in %d is =%d",No,Max);
-    getch();
-    return 0;
-
+   getch();
+   return 0;
 }
