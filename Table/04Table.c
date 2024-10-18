7fcae52d76b8b0d691cfcc4baa6426cc0344aeeb
PrachiJadhav14 <prachijadhav5514@gmail.com>
2024-10-18 20:30:18 +0530

    Table Program

---
+++ Table/04Table.c
@@ -0,0 +1,21 @@
+//////Program To Print 21 TO 13
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int R = 0,C = 0;
+    printf("\n=================**************=============\n\n");
+
+    for (R = 1;R <= 10;R++)
+    {
+        for(C = 21;C >= 13;C--)
+        {
+            printf("%4d",R * C);
+        }
+        printf("\n");
+    }
+     printf("\n=================**************==============");
+     getch();
+     return 0;
+}
+
