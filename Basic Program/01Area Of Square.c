9e8bd94724d574cef5f7b266ecd4c0eff8cf306c
PrachiJadhav14 <prachijadhav5514@gmail.com>
2024-10-20 22:07:56 +0530

    Basic Program

---
+++ Basic Program/01Area Of Square.c
@@ -0,0 +1,13 @@
+///Area Of Square
+#include<stdio.h>
+#include<conio.h>
+int mian()
+{
+    int side;
+    printf("\n Enter Square is => ");
+    scanf("%d",&side);
+     ///side=side*side;
+    printf("\n Area is : %d",side*side);
+    getch();
+    return 0;
+}
---
+++ Basic Program/02Area Of Circle.c
@@ -0,0 +1,14 @@
+///Area Of Circle
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   float Radius=0,pi=3.14;
+   printf("\n Enter Radius => ");
+   scanf("%f",&Radius);
+    Radius=pi*Radius*Radius;
+   printf("\n Area is: %f", Radius,Radius);
+   getch();
+   return 0;
+}
+
