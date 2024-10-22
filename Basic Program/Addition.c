72e211f953a8ec0d8e81d2e16147b6aae71120eb
PrachiJadhav14 <prachijadhav5514@gmail.com>
2024-10-22 21:26:14 +0530

    Addition Program

---
+++ Basic Program/Addition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=0,b=0,Add=0;
+    printf("\n Enter First Numbers => ");
+    scanf("%d%d",&a,&b);
+
+    Add=a+b;
+    printf("\n Addition = %d",Add);
+
+    getch();
+    return 0;
+}
