27d34c49c284cab5fcbb69f640eb6544593d8c2d
PrachiJadhav14 <prachijadhav5514@gmail.com>
2024-12-05 14:12:06 +0530

    Basic Program

--- Basic Program/02Area Of Circle.c
+++ Basic Program/02Area Of Circle.c
@@ -1,17 +1,16 @@
 ///Area Of Circle
-
 #include<stdio.h>
 #include<conio.h>
 int main()
-{
-   float Radius=0,pi=3.14;
-   printf("\n Enter Radius => ");
-   scanf("%f",&Radius);
-   Radius=pi*Radius*Radius;
+    {
+       float Radius=0,pi=3.14;
+       printf("\n Enter Radius => ");
+       scanf("%f",&Radius);
+       Radius=pi*Radius*Radius;
 
-   printf("\n Area is: %f", Radius,Radius);
+       printf("\n Area is: %f", Radius,Radius);
 
-   getch();
-   return 0;
-}
+       getch();
+       return 0;
+    }
 
