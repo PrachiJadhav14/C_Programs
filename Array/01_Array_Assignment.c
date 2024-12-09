072840ad3c9f77449c7f2b93d35e420953ea063e
PrachiJadhav14 <prachijadhav5514@gmail.com>
2024-12-09 19:56:46 +0530

    Array Assignment

--- Array/01_Array_Assignment.c
+++ Array/01_Array_Assignment.c
@@ -4,14 +4,14 @@
 int main()
 {
 
- int Arr[6]={14,55,21,96,94,22};
+     int Arr[6]={14,55,21,96,94,22};
 
- printf("\n Value Of Element 1 is => %d.",Arr[0]);
- printf("\n Value Of Element 2 is => %d.",Arr[1]);
- printf("\n Value Of Element 3 is => %d.",Arr[2]);
- printf("\n Value Of Element 4 is => %d.",Arr[3]);
- printf("\n Value Of Element 5 is => %d.",Arr[4]);
- printf("\n Value Of Element 6 is => %d.",Arr[5]);
+     printf("\n Value Of Element 1 is => %d.",Arr[0]);
+     printf("\n Value Of Element 2 is => %d.",Arr[1]);
+     printf("\n Value Of Element 3 is => %d.",Arr[2]);
+     printf("\n Value Of Element 4 is => %d.",Arr[3]);
+     printf("\n Value Of Element 5 is => %d.",Arr[4]);
+     printf("\n Value Of Element 6 is => %d.",Arr[5]);
 
 
 getch ();
