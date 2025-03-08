001ab932510faa7ff1d251c0a04bdc252572df95
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-03-08 21:24:12 +0530

    Pointer_Program

--- Pointer/01 Pointer.c
+++ Pointer/01 Pointer.c
@@ -3,18 +3,21 @@
 
 int main()
   {
+     //
      int No = 21;
      int *p = &No;
      int **q =&p;
      int ***m = &q;
      int ****x = &m;
      int *****r = &x;
+      int *****ss = &r;
 
       printf("\n%d",&No);
-     printf("\n%d",&p);
-     printf("\n%d",&q);
-     printf("\n%d",&m);
-     printf("\n%d",&x);
+    // printf("\n%d",&p);
+     //printf("\n%d",&q);
+     //printf("\n%d",&m);
+    // printf("\n%d",&x);
+     //
      printf("\n%d",&r);
 
     printf("\n==========\n");
