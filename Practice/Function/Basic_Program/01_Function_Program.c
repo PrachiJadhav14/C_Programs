c3ee98e4420d0156d2ba9bcc52b5479dd12561cb
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-05-05 22:41:10 +0530

    Function

--- Practice/Function/Basic_Program/01_Function_Program.c
+++ Practice/Function/Basic_Program/01_Function_Program.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
 void printHello(); ///Function Declaration / Prototype
-
   int main()
     {
-                        ///Function Call
-
-        printHello();
+        printHello();    ///Function Call
         printHello();
         printHello();
         printHello();
         return 0;
 
     }
-
     void printHello(){             ///Function Defination
       printf("Hello Function \n");
     }
