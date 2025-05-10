41f6d111251111085cec5596be0e3bb8e228bcab
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-05-10 23:29:28 +0530

    String_Copy

---
+++ Practice/String/Library_Function/02_String_Copy.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    char FName[10] = {'\0'};
+    char LName[10] = {};
+
+    printf("\n Enter a 1st Name : ");
+    gets(FName);
+
+    printf("\n Enter 2nd Name : ");
+    gets(LName);
+
+    printf("\n 1st Name is => %s",FName);
+
+    printf("\n 2nd Name is => %s",LName);
+
+    strcpy(FName,LName);
+
+    getch();
+
+    printf("\n\n After Copy 1st Name is = %s....!",FName);
+
+    printf("\n\n After Copy Last Name is = %s $$$",LName);
+
+    getch();
+    return 0;
+}
