3dbcbee406ae66294fde53aebd2d18e5a84df26b
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-05-23 15:44:14 +0530

    String_Length_Main

--- Practice/String/String_Main_Function/03_String_Length_In_Main.c
+++ Practice/String/String_Main_Function/03_String_Length_In_Main.c
@@ -1,30 +1,33 @@
 #include<stdio.h>
 #include<conio.h>
 
-int String_Length(char[]);
+int lengthCase(char[]);
 
 int main()
-{
-    char cSrc[20] = "";
-    int i = 0;
 
-    printf("\n Enter A String : ");
+{
+    int cSrc[20] = {'\0'};
+     int i = 0;
+
+    printf("\n Enter A String => ");
     gets(cSrc);
 
-    i = String_Length(cSrc);
-    printf("\n StringLength of  String is = %d",i);
+   i= lengthCase(cSrc);
 
-    getch();
-    return 0;
+    printf("\n You're String Length is = %d",i);
+
+getch();
+return 0;
 }
-
-int String_Length(char cSrc[])
+int lengthCase(char cSrc[])
 {
-    int i = 0;
+    int i =0;
 
-    while(cSrc[i] != '\0')
+        while(cSrc[i] != '\0')
     {
         i++;
     }
     return i;
+
 }
+
