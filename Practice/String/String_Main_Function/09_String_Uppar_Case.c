b61aa62b701448a9b226408804892c9ee035b61b
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-05-28 22:59:10 +0530

    String_Uppar_Case

---
+++ Practice/String/String_Main_Function/09_String_Uppar_Case.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+struprCase(cSrc);
+
+int main()
+{
+    char cSrc [20] = {'\0'};
+
+    printf("\n Enter A String => ");
+    gets(cSrc);
+
+   struprCase(cSrc);      //Function Call
+
+    printf("\n\n After A string Convert To UpperCase is = %s",cSrc);
+
+   getch();
+  return 0;
+
+}
+
+int struprCase(char src[])
+{
+    int i = 0;
+
+    while(src[i] != '\0')
+    {
+        if(src[i] >= 'a' && src[i]<= 'z')
+        {
+            src[i] = src[i]-32;
+        }
+        i++;
+    }
+    return 0;
+}
