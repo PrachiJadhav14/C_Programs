c47857e9d63c42301458f8e60ddd2f716b42cace
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-05-30 22:48:58 +0530

    String_Main_Function_Reverce

---
+++ Practice/String/String_Main_Function/07_B_String_Reverce.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+
+int StrlenF(char *);
+void StrRevF(char *);
+
+int main()
+{
+    char cSrc[20] = {'\0'};
+
+    printf("\n Enter A String => ");
+    gets(cSrc);
+
+    StrRevF(cSrc);
+
+    printf("\n\n Reversed String is :%s",cSrc);
+
+    getch();
+    return 0;
+
+}
+
+int StrlenF(char *Src)
+{
+    int Cnt = 0;
+    while(*Src != '\0')
+    {
+        Src++;
+        Cnt++;
+    }
+    return Cnt;
+}
+
+void StrRevF(char *Str)
+{
+    char ch = '\0';
+    char *Temp = Str + StrlenF(Str)-1;
+
+    while(Str < Temp)
+    {
+        ch = *Str;
+        *Str = *Temp;
+        *Temp = ch;
+
+        Str++;
+        Temp--;
+    }
+    return ;
+}
